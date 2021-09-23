4a6d25dad8c3a909b3441e93c5e42bd13280138f
Chandan <chandanmohanty11111>
2021-09-23 08:42:43 +0530

    my name is chandan

--- src/firstproject.java
+++ src/firstproject.java
@@ -3,8 +3,8 @@ public class firstproject {
     public static void main(String[] args){
         System.out.println("Welcome to Java");
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello ");
+
+
 
     }
 }
